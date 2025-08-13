d65969fe95914d40bd627698dc4d4ea60ab2d0e9
kekekuli <kekekuliobj@gmail.com>
2025-08-13 15:23:13 +0800

    update vue3 lsp config

--- lua/configs/custom_lspconfig.lua
+++ lua/configs/custom_lspconfig.lua
@@ -47,6 +47,7 @@ if (vue_version == 3) then
   -- For Mason v2,
   local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
       '/vue-language-server' .. '/node_modules/@vue/language-server'
+  local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
   local vue_plugin = {
     name = '@vue/typescript-plugin',
     location = vue_language_server_path,
@@ -63,12 +64,22 @@ if (vue_version == 3) then
         },
       },
     },
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    filetypes = tsserver_filetypes
   }
+  local ts_ls_config = {
+    init_options = {
+      plugins = {
+        vue_plugin,
+      },
+    },
+    filetypes = tsserver_filetypes,
+  }
+
   -- If you are on most recent `nvim-lspconfig`
   local vue_ls_config = {}
-  -- nvim 0.11 or above
+
   vim.lsp.config('vtsls', vtsls_config)
   vim.lsp.config('vue_ls', vue_ls_config)
-  vim.lsp.enable({ 'vtsls', 'vue_ls' }, vue_version == 3)
+  vim.lsp.config('ts_ls', ts_ls_config)
+  vim.lsp.enable({ 'ts_ls', 'vue_ls' }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
 end
